4a3af501a10e588684659d453ebb49d6b5d00506
Osama-Anmar <90341706+Osama-Anmar@users.noreply.github.com>
2023-10-17 23:15:27 +0300

    Add files via upload

---
+++ Arabic Sentiment Analysis/source/sentimentinformation.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def sentiment_percentage(data, target, figsize):
+    fig, ax = plt.subplots(figsize = figsize)
+    ax.pie(data[target].value_counts(), 
+       labels=data[target].unique(),
+       colors=['#0e3a06','#21830e'], 
+       autopct='%1.1f%%',
+       textprops = {'size': 'large'})
+    plt.title('{} Percentage'.format(target))
+    return plt.show()
+
+def sentiment_counts(data, target, figsize):
+    ax, fig = plt.subplots(figsize = figsize)
+    ax =sns.countplot(x=target, data=data, dodge=False, hue=target,  order= data[target].value_counts().index, hue_order =data[target].value_counts().index,  palette=['#dcf4cb', '#afe586'], legend='full');
+    ax.set(xticklabels=[]) 
+    ax.set(ylabel=None) 
+    plt.title('{} Count'.format(target))
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    return plt.show()
