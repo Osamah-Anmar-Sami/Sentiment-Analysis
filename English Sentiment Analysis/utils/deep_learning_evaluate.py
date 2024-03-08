260258ae1ea6927d00bba0d25f029c105f832d14
Osama Anmar Sami <90341706+Osama-Anmar@users.noreply.github.com>
2024-03-08 22:06:56 +0300

    Delete English Sentiment Analysis/utils directory

--- English Sentiment Analysis/utils/deep_learning_evaluate.py
+++
@@ -1,4 +0,0 @@
-def deep_learning_evaluate_(model, x, y, train_test):
-           loss, accuracy = model.evaluate(x, y, verbose=False)
-           print(" ")
-           print('The Evaluation Of {} Is {:.4f} For Loss And {:.4f} For Accuracy'.format(train_test, loss,  accuracy))
--- English Sentiment Analysis/utils/most_frequent_word_plot.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-def bar_plot(data, x, y):
-    fig, ax = plt.subplots(figsize=(20, 10))
-    sns.barplot(data=data, x=x, y=y, width=0.6, palette = sns.color_palette("husl", 15), ax=ax)
-    plt.rcParams['font.family'] = 'Arial'
