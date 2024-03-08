94a9bf1a8f4c42f2ae5bff549303b98fd23bee63
Osama Anmar Sami <90341706+Osama-Anmar@users.noreply.github.com>
2024-03-08 23:35:24 +0300

    Add files via upload

--- Arabic Sentiment Analysis/utils/generate_poitive_negative.py
+++ Arabic Sentiment Analysis/utils/generate_poitive_negative.py
@@ -2,7 +2,17 @@ import numpy as np
 
 
 def false_true_positive(Confusin_Matrix):
+        """
+    Calculates the number of False Positives and True Positives from a confusion matrix.
 
+    Args:
+        confusion_matrix (np.ndarray): A 2D array representing a confusion matrix, where rows represent actual classes and columns represent predicted classes.
+
+    Returns:
+        tuple: A tuple containing two elements:
+            - False positives (int): the total number of False Positives.
+            - True positives (int): the total number of True Positives.
+    """
         False_Postive = Confusin_Matrix.sum(axis=0) - np.diag(Confusin_Matrix)
         False_Postive = False_Postive.astype(int)
         False_Postive = sum(False_Postive)
@@ -16,7 +26,17 @@ def false_true_positive(Confusin_Matrix):
 
 
 def false_true_negative(Confusin_Matrix):
+        """
+        calculates the number of False Negatives and True Negatives from a confusion matrix.
 
+        Args:
+                confusion_matrix (np.ndarray): A 2D array representing a confusion matrix, where rows represent actual classes and columns represent predicted classes.
+
+        Returns:
+                tuple: A tuple containing two elements:
+                - False negatives (int): the total number of False Negatives
+                - True negatives (int): the total number of True Negatives
+    """
         False_Negative = Confusin_Matrix.sum(axis=1) - np.diag(Confusin_Matrix)
         False_Negative = False_Negative.astype(int)
         False_Negative = sum(False_Negative)
--- Arabic Sentiment Analysis/utils/machinelearning.py
+++ Arabic Sentiment Analysis/utils/machinelearning.py
@@ -1,14 +1,28 @@
-import pandas as pd
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import matplotlib.pyplot as plt
-
 def machine_learning_model(algorithm):
+        """
+                this function simply returns the provided algorithm as-is
+
+                Args:
+                        algorithm (object): any object representing a machine learning model
+
+                Returns:
+                        object: the input algorithm object
+                """
         model = algorithm
         return model
         
-
 def fit_model(model, X_train, y_train):
+        """
+                fits (trains) a provided machine learning model on the given training data
+
+        Args:
+                model (object): the machine learning model to be trained. It should have a `fit` method that takes training data features and labels as arguments.
+                X_train (np.ndarray): training data features
+                y_train (np.ndarray): training data labels
+
+        Returns:
+                object: The trained model object.
+        """
         model.fit(X_train, y_train)
         return model
         
